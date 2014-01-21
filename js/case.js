ec5df54c7b3398f8c4a3547ba54446587105318f
Kirkhoff <curt.kirkhoff@audaxhealth.com>
2014-01-20 22:29:25 -0500

    added Parallax scrolling to Our Work image

--- js/case.js
+++ js/case.js
@@ -1,4 +1,8 @@
-angular.module('case', ['zen.ui.select'])
+angular.module('case', ['zen.ui.select', 'ui.bootstrap.position', 'angular.stellar'])
+    .config(['stellarConfig', function(stellarConfig){
+        stellarConfig.horizontalScrolling = false;
+        stellarConfig.verticalOffset = 300;
+    }])
     .controller('scheduleCtrl', function($scope){
         // Options array for the Best Time select input
         $scope.timeOptions = [
