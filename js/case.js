3e6bba1ea8f77887a197ae6da710e54058222211
Kirkhoff <curt.kirkhoff@audaxhealth.com>
2014-01-23 01:41:22 -0500

    added sub nav

--- js/case.js
+++ js/case.js
@@ -3,28 +3,8 @@ angular.module('case', ['ngSanitize', 'zen.ui.select', 'ui.bootstrap', 'angular.
         stellarConfig.horizontalScrolling = false;
         stellarConfig.verticalOffset = -300;
     }])
-    .controller('caseCtrl', function($scope, $location, $anchorScroll){
-        $scope.scrollTo = function(elementId){
-            $location.hash(elementId);
-            $anchorScroll();
-        };
-    })
-    .controller('scheduleCtrl', function($scope){
-        // Options array for the Best Time select input
-        $scope.timeOptions = [
-            'Best Time to Reach Me',
-            '8AM-10PM',
-            '10AM-12PM',
-            '12PM-1PM',
-            '1PM-3PM',
-            '3PM-5PM',
-            'Other'];
-
-        // The default choice for the Best Time select input
-        $scope.bestTime = $scope.timeOptions[0];
-    })
-    .controller('heroCtrl', function($scope){
-        $scope.slides = [{
+    .constant('HERO_SLIDES', [
+        {
             header: 'Kitchens',
             img: '/img/carousel/kitchens.jpg',
             description1: '<mark>Lorem ipsum dolor sit amet conse ctetuer</mark>',
@@ -59,12 +39,136 @@ angular.module('case', ['ngSanitize', 'zen.ui.select', 'ui.bootstrap', 'angular.
             description2: '<mark>adipiscing elit</mark>, sed diam nonummy nibh',
             description3: 'eusmod tincidunt ut laoreet.',
             active: false
-        }];
+        }])
+    .constant('OUR_WORK', [
+        {
+            name: 'Kitchens',
+            url: '#',
+            img: '/img/sub-nav/kitchens.jpg'
+        },{
+            name: 'Baths',
+            url: '#',
+            img: '/img/sub-nav/baths.jpg'
+        },{
+            name: 'Interiors',
+            url: '#',
+            img: '/img/sub-nav/interiors.jpg'
+        },{
+            name: 'Exteriors',
+            url: '#',
+            img: '/img/sub-nav/exteriors.jpg'
+        },{
+            name: 'Additions',
+            url: '#',
+            img: '/img/sub-nav/additions.jpg'
+        }])
+    .constant('OUR_PEOPLE', [
+        {
+            name: 'Craftsmen',
+            url: '#',
+            img: '/img/sub-nav/baths.jpg'
+        },{
+            name: 'Designers',
+            url: '#',
+            img: '/img/sub-nav/interiors.jpg'
+        },{
+            name: 'Project Managers',
+            url: '#',
+            img: '/img/sub-nav/exteriors.jpg'
+        },{
+            name: 'Project Developers',
+            url: '#',
+            img: '/img/sub-nav/additions.jpg'
+        }])
+    .constant('IDEA_CENTER', [
+        {
+            name: 'Kitchens',
+            url: '#',
+            img: '/img/sub-nav/kitchens.jpg'
+        },{
+            name: 'Baths',
+            url: '#',
+            img: '/img/sub-nav/baths.jpg'
+        },{
+            name: 'Interiors',
+            url: '#',
+            img: '/img/sub-nav/interiors.jpg'
+        },{
+            name: 'Exteriors',
+            url: '#',
+            img: '/img/sub-nav/exteriors.jpg'
+        },{
+            name: 'Additions',
+            url: '#',
+            img: '/img/sub-nav/additions.jpg'
+        }])
+    .controller('caseCtrl', function($scope, $location, $anchorScroll){
+        $scope.scrollTo = function(elementId){
+            $location.hash(elementId);
+            $anchorScroll();
+        };
+    })
+    .controller('scheduleCtrl', function($scope){
+        // Options array for the Best Time select input
+        $scope.timeOptions = [
+            'Best Time to Reach Me',
+            '8AM-10PM',
+            '10AM-12PM',
+            '12PM-1PM',
+            '1PM-3PM',
+            '3PM-5PM',
+            'Other'];
 
+        // The default choice for the Best Time select input
+        $scope.bestTime = $scope.timeOptions[0];
+    })
+    .controller('heroCtrl', function($scope, HERO_SLIDES){
+        $scope.slides = HERO_SLIDES;
         $scope.nextImage = function(index){
             var nextIndex = (index + 1) % $scope.slides.length;
             return $scope.slides[nextIndex].img;
         };
+    })
+    .controller('subNavCtrl', function($scope, $timeout, OUR_WORK, OUR_PEOPLE, IDEA_CENTER){
+        var ourWorkTimeout,
+            ourPeopleTimeout,
+            ideaCenterTimeout;
+
+        $scope.ourWork = {
+            nav: OUR_WORK,
+            collapsed: true
+        };
+        $scope.ourPeople = {
+            nav: OUR_PEOPLE,
+            collapsed: true
+        };
+        $scope.ideaCenter = {
+            nav: IDEA_CENTER,
+            collapsed: true
+        };
+
+        $scope.ourWorkCollapsed = true;
+        $scope.ourPeopleCollapsed = true;
+        $scope.ideaCenterCollapsed = true;
+
+        $scope.open = function(section){
+            closeAllExcept(section)
+            $timeout.cancel(section.timeout);
+        };
+
+        $scope.close = function(section){
+            section.timeout = $timeout(function(){
+                section.collapsed = true;
+            }, 500);
+        };
+
+        function closeAllExcept(section){
+            $scope.ourWork.collapsed = ($scope.ourWork.collapse != section);
+            $scope.ourPeople.collapsed = ($scope.ourPeople.collapse != section);
+            $scope.ideaCenter.collapsed = ($scope.ideaCenter.collapse != section);
+
+            section.collapsed = false;
+        }
     })
     .directive('scrollTo', function(){
         return {
--- js/vendor/ui-bootstrap-custom-0.10.0.js
+++ js/vendor/ui-bootstrap-custom-0.10.0.js
@@ -2,10 +2,10 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 0.10.0 - 2014-01-22
+ * Version: 0.10.0 - 2014-01-23
  * License: MIT
  */
-angular.module("ui.bootstrap", ["ui.bootstrap.transition","ui.bootstrap.carousel","ui.bootstrap.position"]);
+angular.module("ui.bootstrap", ["ui.bootstrap.transition","ui.bootstrap.carousel","ui.bootstrap.collapse","ui.bootstrap.position"]);
 angular.module('ui.bootstrap.transition', [])
 
 /**
@@ -434,6 +434,82 @@ function CarouselDemoCtrl($scope) {
     }
   };
 }]);
+
+angular.module('ui.bootstrap.collapse', ['ui.bootstrap.transition'])
+
+  .directive('collapse', ['$transition', function ($transition, $timeout) {
+
+    return {
+      link: function (scope, element, attrs) {
+
+        var initialAnimSkip = true;
+        var currentTransition;
+
+        function doTransition(change) {
+          var newTransition = $transition(element, change);
+          if (currentTransition) {
+            currentTransition.cancel();
+          }
+          currentTransition = newTransition;
+          newTransition.then(newTransitionDone, newTransitionDone);
+          return newTransition;
+
+          function newTransitionDone() {
+            // Make sure it's this transition, otherwise, leave it alone.
+            if (currentTransition === newTransition) {
+              currentTransition = undefined;
+            }
+          }
+        }
+
+        function expand() {
+          if (initialAnimSkip) {
+            initialAnimSkip = false;
+            expandDone();
+          } else {
+            element.removeClass('collapse').addClass('collapsing');
+            doTransition({ height: element[0].scrollHeight + 'px' }).then(expandDone);
+          }
+        }
+
+        function expandDone() {
+          element.removeClass('collapsing');
+          element.addClass('collapse in');
+          element.css({height: 'auto'});
+        }
+
+        function collapse() {
+          if (initialAnimSkip) {
+            initialAnimSkip = false;
+            collapseDone();
+            element.css({height: 0});
+          } else {
+            // CSS transitions don't work with height: auto, so we have to manually change the height to a specific value
+            element.css({ height: element[0].scrollHeight + 'px' });
+            //trigger reflow so a browser realizes that height was updated from auto to a specific value
+            var x = element[0].offsetWidth;
+
+            element.removeClass('collapse in').addClass('collapsing');
+
+            doTransition({ height: 0 }).then(collapseDone);
+          }
+        }
+
+        function collapseDone() {
+          element.removeClass('collapsing');
+          element.addClass('collapse');
+        }
+
+        scope.$watch(attrs.collapse, function (shouldCollapse) {
+          if (shouldCollapse) {
+            collapse();
+          } else {
+            expand();
+          }
+        });
+      }
+    };
+  }]);
 
 angular.module('ui.bootstrap.position', [])
 
